86792338117ee407ef58aff1b41b3b38ca5a9c6c
tahazz5 <130492908+tahazz5@users.noreply.github.com>
2025-02-20 20:18:33 +0100

    Update vite.config.js

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/OmarOukhtite/', // Remplacez "mon-portfolio" par le nom de votre dépôt GitHub
+  base: '/',  // Utilisez la racine si le site est déployé à la racine
 });
